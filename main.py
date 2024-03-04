7689bf9c083936fe4fdc65cc54dfdf8109097e55
Sergei <107009267+dangerousfroggo@users.noreply.github.com>
2024-03-04 15:01:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#created by Sergei Borishchev, 349512905
+
+def caeserCypher(string,key):
+  stringArray = [x for x in string]
+  shiftedString = ""
+  for i in range(len(stringArray)):
+    letter = stringArray[i]
+    shiftedString += chr(ord(letter) + key)
+  return shiftedString
+print(caeserCypher("hello", 3))
+  
